0d1ad0b9f2002b7433faffcabc8bee9b218717b3
Scott Harp <ssharp9999@gmail.com>
2023-07-10 20:54:05 -0700

    Read CRUD operation functional, integrated end-to-end, and tested with basic ui

--- app.js
+++ app.js
@@ -3,6 +3,63 @@ console.log('connected')
 // local storage serves as database
 const localStorage = window.localStorage
 
+/*
+READ (cRud) operation and helper functions
+*/
+
+// Function to get and return all projects from local storage db
+const getAllProjects = () => {
+ const savedProjects = JSON.parse(localStorage.getItem('projects')) || []
+ return savedProjects
+}
+
+// Function to read and display all projects
+const displayAllProjects = () => {
+ // get all projects from db
+ const savedProjects = getAllProjects()
+ // if there are projects, insert the record in the table for display
+ if (savedProjects.length !== 0) {
+  savedProjects.forEach(project => {
+   insertProjectRecord(project)
+  })
+ }
+}
+
+// function to insert a row into the projects table with the provided project details
+const insertProjectRecord = (project) => {
+
+ // get the project table element and insert a row at the end
+ const projectTable = document.getElementById("projectsTable")
+ let row = projectTable.insertRow(-1)
+
+ // insert all the cells to display data
+ let projectID = row.insertCell(0)
+ let projectName = row.insertCell(1)
+ let projectStartDate = row.insertCell(2)
+ let projectEndDate = row.insertCell(3)
+ let projectPriority = row.insertCell(4)
+ let projectStatus = row.insertCell(5)
+ let projectAtRisk = row.insertCell(6)
+ let projectOwner = row.insertCell(7)
+ let projectNotes = row.insertCell(8)
+
+ // display the associated db value from the project object
+ projectID.innerText = project.projectID
+ projectName.innerText = project.projectName
+ projectStartDate.innerText = project.projectStartDate
+ projectEndDate.innerText = project.projectEndDate
+ projectPriority.innerText = project.projectPriority
+ projectStatus.innerText = project.projectStatus
+ projectAtRisk.innerText = project.projectAtRisk
+ projectOwner.innerText = project.projectOwner
+ projectNotes.innerText = project.projectNotes
+
+}
+
+/*
+Seed Hard Coded Data for Testing
+*/
+
 // Function to generate seed data from hardcoded information
 const generateSeedData = () => {
 
@@ -20,8 +77,8 @@ const generateSeedData = () => {
 
  // for each project, create object and set it to local storage database
  for (let i = 0; i < numberOfProjects; i++) {
-  // get the saved projects, if empty start with empty array
-  const savedProjects = JSON.parse(localStorage.getItem('projects')) || []
+  // get all current projects to add current project
+  const savedProjects = getAllProjects()
   // create the object
   const project = {
    "projectID": projectIDs[i],
@@ -40,6 +97,13 @@ const generateSeedData = () => {
  }
 }
 
+/*
+Event Listeners
+*/
+
 // add event listener for the button to genereate seed data
 let genSeedDataBtn = document.getElementById("genSeedDataBtn")
 genSeedDataBtn.addEventListener("click", generateSeedData)
+
+// READ and display all project on the HTML
+document.addEventListener("DOMContentLoaded", displayAllProjects())
